127455afdbd7b6c79dfddf7196c1d39f7491f813
Gian Sass <gian.sass@outlook.de>
2017-08-01 13:40:35 +0200

    Fix missing space

--- ntop.c
+++ ntop.c
@@ -913,7 +913,7 @@ static void WriteProcessInfo(const process *Process, BOOL Highlighted)
 
 		CharsWritten += _tprintf(_T("%s"), Process->ExeName);
 	} else {
-		CharsWritten = _tprintf(_T("\n%6u  %9s  %3u  %04.1f%%  % 6.1f MB  %4u  %s %s"),
+		CharsWritten = _tprintf(_T("\n%6u  %9s  %3u  %04.1f%%  % 6.1f MB  %4u  %s  %s"),
 				Process->ID,
 				Process->UserName,
 				Process->BasePriority,
