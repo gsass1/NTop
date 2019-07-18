5387ac7af5d58b5501ad2ef8db0a705fefb364c7
Gian Sass <gian.sass@outlook.de>
2019-07-18 11:08:42 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,10 +5,14 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 IF NOT EXIST build mkdir build
 pushd build
 
-REM Debug build
-cl -W4 /GA /MT /Z7 ..\ntop.c ..\util.c ..\vi.c Advapi32.lib User32.lib
-
-REM Release build
-REM cl -W4 /GA /MT /O2 ..\ntop.c ..\util.c ..\vi.c Advapi32.lib User32.lib
+IF "%~1"=="-release" (
+	REM Release build
+    echo Release build
+	cl -W4 /GA /MT /O2 ..\ntop.c ..\util.c ..\vi.c Advapi32.lib User32.lib
+) else (
+    REM Debug build
+    echo Debug build
+    cl -W4 /GA /MT /Z7 ..\ntop.c ..\util.c ..\vi.c Advapi32.lib User32.lib
+)
 
 popd
