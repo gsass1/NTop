cf66cb8e73023b1c77a9a9ef124112ad007175b9
Máté Ferenc Nagy-Egri <mate@streamhpc.com>
2021-08-14 16:48:47 +0200

    Test code cleanup

--- main.cpp
+++ main.cpp
@@ -38,8 +38,6 @@ int main()
             nullptr,
             EOAC_NONE));
 
-        //std::vector<int> loads(std::thread::hardware_concurrency());
-
         auto refresher = winrt::create_instance<IWbemRefresher>(CLSID_WbemRefresher);
         winrt::com_ptr<IWbemConfigureRefresher> config;
         winrt::check_hresult(refresher->QueryInterface(config.put()));
@@ -82,9 +80,6 @@ int main()
                     reinterpret_cast<IWbemObjectAccess**>(enum_accessors.data()),
                     &count));
 
-                //if (hresult != WBEM_E_BUFFER_TOO_SMALL)
-                //    throw std::runtime_error{"Expecting hresult == WBEM_E_BUFFER_TOO_SMALL and count == 1 while obtaining PercentProcessorTime property handle."};
-
                 CIMTYPE handle, type;
                 winrt::check_hresult(enum_accessors[0]->GetPropertyHandle(
                     L"PercentProcessorTime",
@@ -95,7 +90,6 @@ int main()
             }();
 
         std::vector<winrt::com_ptr<IWbemObjectAccess>> enum_accessors(logical_core_count);
-        //std::wstring percent_processor_time_string(4, '\0');
         std::uint64_t percent_processor_time;
         for(int I = 0 ; I < 10 ; ++I)
         {
@@ -109,69 +103,22 @@ int main()
                 reinterpret_cast<IWbemObjectAccess**>(enum_accessors.data()),
                 &count));
 
-            for (auto& enum_access : enum_accessors)
+            // Loop over core loads. (Last entry seems to be an avarage of all cores.)
+            for (size_t i = 0 ; i < enum_accessors.size() - 1 ; ++i)
             {
-                BSTR* type_name = nullptr;
-                CIMTYPE prop_type;
-                winrt::check_hresult(enum_access->GetPropertyInfoByHandle(
-                    percent_processor_time_handle,
-                    type_name,
-                    &prop_type
-                ));
-
                 long bytes_read;
-                winrt::check_hresult(enum_access->ReadPropertyValue(
+                winrt::check_hresult(enum_accessors[i]->ReadPropertyValue(
                     percent_processor_time_handle,
                     sizeof(std::uint64_t),
                     &bytes_read,
                     reinterpret_cast<byte*>(&percent_processor_time)));
 
                 std::cout << percent_processor_time << std::endl;
-
-                //wil::unique_variant percent;
-                //winrt::check_hresult(enum_access->Get( L"PercentProcessorTime", 0, percent.addressof(), nullptr, nullptr));
-                //std::cout << std::stoi(percent.bstrVal) << std::endl;
-
             }
 
             std::cout << std::endl;
             std::this_thread::sleep_for(std::chrono::seconds{1});
         }
-/*
-        winrt::com_ptr<IEnumWbemClassObject> enumerator;
-        winrt::check_hresult(services->ExecQuery(
-            L"WQL",
-            L"SELECT PercentProcessorTime FROM Win32_PerfFormattedData_PerfOS_Processor WHERE Name <> '_Total'",
-            WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
-            nullptr,
-            enumerator.put())
-        );
-
-        //while (enumerator)
-        for (size_t i = 0 ; i < loads.size() ; ++i)
-        {
-            winrt::com_ptr<IWbemClassObject> class_object;
-            ULONG ret;
-            winrt::check_hresult(enumerator->Next(
-                WBEM_INFINITE,
-                1,
-                class_object.put(),
-                &ret)
-            );
-
-            if (ret == 0) { break; }
-
-            wil::unique_variant percent;
-            winrt::check_hresult(class_object->Get( L"PercentProcessorTime", 0, percent.addressof(), nullptr, nullptr));
-
-            loads.at(i) = std::stoi(percent.bstrVal);
-        }
-
-        for(auto& load : loads) std::cout << load << "\n";
-        std::cout << std::endl;
-
-        std::this_thread::sleep_for(std::chrono::milliseconds{500});
-*/
     }
     catch(winrt::hresult_error& err)
     {
